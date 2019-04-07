747ee2de797596ee754f018fd9e2406f90d2c63a
Hannes <hanestolp@gmail.com>
2019-04-07 22:31:20 +0200

    Aufgabe1_2 & cmake

---
+++ source/Aufgabe_1.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int smlInt = 1;
+
+int main()
+{
+            
+     for( int i = 1; i <= 20; i++) 
+     {
+        if((i = 20) && (smlInt % i == 0))
+        {
+             std::cout << smlInt;
+            break;
+        }
+             
+        if(smlInt % i != 0)
+        {
+            smlInt++;
+            i = 0;
+        }                
+    }                      
+    return 0;
+}
+
+
+
